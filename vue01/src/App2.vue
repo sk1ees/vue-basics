a204e4a04d71cbfc3373fbd6af25eeffa9963f4a
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-10-25 23:19:31 +0530

    form-control

--- vue01/src/App2.vue
+++ vue01/src/App2.vue
@@ -7,7 +7,8 @@ import { ref } from "vue";
 
 const status = ref("Active");
 const name = ref("Deep");
-
+const tasks = ref([]);
+const formHandle = ref("");
 const handleClick = () => {
   if (status.value === "Active") {
     status.value = "Inactive";
@@ -16,9 +17,24 @@ const handleClick = () => {
   }
   console.log(status.value);
 };
+const addTask = () => {
+  tasks.value.push(formHandle.value);
+  formHandle.value = "";
+};
 </script>
+
 <template>
-  <h1>{{ name }}</h1>
+  <!-- <h1>{{ name }}</h1>
   <h1>{{ status }}</h1>
-  <button @click="handleClick">Click Me</button>
+  <button @click="handleClick">Click Me</button> -->
+
+  <form @submit.prevent="addTask">
+    <label for="textInput">Create your TODO List</label><br />
+    <input type="text" id="textInput" v-model="formHandle" /><br />
+    <input type="submit" value="Add Item" />
+  </form>
+  <br />
+  <ul v-for="task in tasks">
+    <li>{{ task }}</li>
+  </ul>
 </template>
