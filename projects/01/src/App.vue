24a3f4781eb279e417bba2a4c807949e221d632c
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-10-29 22:43:23 +0530

    vue-router

--- projects/01/src/App.vue
+++ projects/01/src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-import Hero from "./components/Hero.vue";
-import HomeCards from "./components/HomeCards.vue";
-import JobListings from "./components/JobListings.vue";
+import { RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <Navbar />
-  <Hero title="Hello this is Prop" />
-  <HomeCards />
-
-  <JobListings :limit="3" :showButton="true"/>
-
+  <RouterView />
 </template>
