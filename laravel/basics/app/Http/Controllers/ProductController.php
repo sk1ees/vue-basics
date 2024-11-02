63b32d37548a2ee5010d512d0a0ce9c88f40b171
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-11-02 21:19:51 +0530

    validation

--- laravel/basics/app/Http/Controllers/ProductController.php
+++ laravel/basics/app/Http/Controllers/ProductController.php
@@ -26,6 +26,15 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
+
+
+        $request->validate([
+
+            'name' => 'required|max:100',
+            'description' => 'nullable|min:3',
+            'size' => 'required|decimal:0,2|max:100',
+
+        ]);
         Product::create($request->input());
         return redirect()->route('products.index');
     }
--- laravel/basics/resources/views/products/create.blade.php
+++ laravel/basics/resources/views/products/create.blade.php
@@ -2,18 +2,26 @@
 
     <h1>New Product</h1>
 
+    @if ($errors->any())
 
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+    </ul>
+
+    @endif
 
     <form method="post" action="{{route('products.store')}}">
         @csrf
 
         <label for="name">Name</label>
-        <input type="text" name="name" id="name">
+        <input type="text" name="name" id="name" value="{{old('name')}}">
 
         <label for="description">Description</label>
-        <textarea name="description" id="description"></textarea>
+        <textarea name="description" id="description">{{old('description')}}</textarea>
 
-        <label for="size">Size</label>
+        <label for="size" value="{{old('size')}}">Size</label>
         <input type="text" name="size" id="size">
 
 
